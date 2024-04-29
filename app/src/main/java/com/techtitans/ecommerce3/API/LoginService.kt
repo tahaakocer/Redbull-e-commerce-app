dc5ee844a65aa9750e61dc191663e4bb716b87ed
tahaakocer <taha.kocer317@gmail.com>
2024-04-29 07:41:27 +0300

    login register completed

--- app/src/main/java/com/techtitans/ecommerce3/API/LoginService.kt
+++ app/src/main/java/com/techtitans/ecommerce3/API/LoginService.kt
@@ -1,5 +1,6 @@
 package com.techtitans.ecommerce3.API
 
+import com.techtitans.ecommerce3.Model.LoginResponse
 import com.techtitans.ecommerce3.Model.UserModel
 import okhttp3.ResponseBody
 import retrofit2.Call
@@ -11,6 +12,15 @@ import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface LoginService {
+
+    // Method to handle user login
+    @FormUrlEncoded
+    @POST("user/login")
+    fun loginUser(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<LoginResponse>  // Use LoginResponse class to parse the response
+
     @FormUrlEncoded
     @POST("user/register")
     fun registerUser(
